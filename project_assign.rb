fd3587515dfccf2d5d411a13e6a12ddc85ac1e35
Nathan R Smith <nrsmith012@gmail.com>
2017-10-03 19:55:26 -0700

    first commit

---
+++ project_assign.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_accessor :name
+  attr_accessor :description
+  def initialize(name, description)
+    @name = name
+    @description = description
+  end
+  def elevator_pitch
+    puts "#{self.name}, #{self.description}"
+  end
+end
+project1 = Project.new("Project 1", "Description 1")
+puts project1.name # => "Project 1"
+project1.elevator_pitch  # => "Project 1, Description 1"
