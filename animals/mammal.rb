112e8372f9b8368775f7c7c57916c91153c20326
Nathan R Smith <nrsmith012@gmail.com>
2017-10-03 22:19:38 -0700

    flying lion

--- animals/mammal.rb
+++ animals/mammal.rb
@@ -10,6 +10,10 @@ class Mammal
   def showhealth
     puts @health
   end
+
+  def changehealth hp_change
+    @health += hp_change
+  end
 end
 
 class Dog < Mammal
@@ -24,17 +28,42 @@ class Dog < Mammal
   def run
     changehealth(-10)
   end
-  
-  def changehealth hp_change
-    @health += hp_change
-  end
 
 end
 
-catfish = Dog.new
-catfish.walk
-catfish.walk
-catfish.run
-catfish.run
-catfish.pet
-catfish.showhealth
+# catfish = Dog.new
+# catfish.walk
+# catfish.walk
+# catfish.run
+# catfish.run
+# catfish.pet
+# catfish.showhealth
+class Lion < Mammal
+  def initialize
+    @health = 170
+  end
+
+  def fly
+    changehealth(-10)
+  end
+  def attack_town
+    changehealth(-50)
+  end
+  def eat_humans
+    changehealth(20)
+  end
+  def showhealth
+    puts "This is a lion"
+    super
+  end
+end
+
+lassie = Lion.new
+puts 'lassie.attack_town [x3]'
+for i in 1..3
+  lassie.attack_town
+end
+puts 'lassie.eat_humans [x2]'
+lassie.eat_humans
+lassie.eat_humans
+lassie.showhealth
